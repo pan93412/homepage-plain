efc1700c871a14009f8d7062b72320490e0b356d
Yi-Jyun Pan <pan93412@gmail.com>
2025-01-05 01:30:33 +0800

    refactor: Use Vite to bundle JavaScript

---
+++ src/email.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+  const REVDT0RFX1RISVNfVEVYVF9BTkRfRklORF9NRV9UT19HRVRfQV9XVEZfOik_ = "bWVAcGFuOTMuY29t";
+
+  for (const $el of document.getElementsByTagName("x-email")) {
+    const e = atob(
+      REVDT0RFX1RISVNfVEVYVF9BTkRfRklORF9NRV9UT19HRVRfQV9XVEZfOik_,
+    );
+    $el.innerHTML = `<a href="mailto:${e}">${e}</a>`;
+  }
+}, Math.random() * 1145.14);
