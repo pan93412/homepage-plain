1efc197031df42d804a32f47b881b233f3b249aa
Yi-Jyun Pan <me@pan93.com>
2025-09-05 20:34:46 +0800

    refactor: migrate to general Nix devshell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              node2nix
+              nodejs
+              nodePackages.pnpm
+            ];
+          };
+        }
+      );
+    };
+}
